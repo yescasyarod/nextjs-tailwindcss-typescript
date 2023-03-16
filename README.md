f8653e416713971889c2b00da136492cedbfd6ed
yescasyarod <42419496+yescasyarod@users.noreply.github.com>
2023-03-16 16:40:50 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-tailwindcss-typescript
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-apeajf)
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <div className="bg-slate-700 h-screen"></div>
+    </>
+  );
+}
